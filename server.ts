0c2476e2ba0d6e4b8f6b6d6e30d902151f4eff6a
zomblic <c.robinwork@outlook.com>
2025-02-27 23:49:07 -0500

    created dt, tables and prompts

---
+++ server.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { QueryResult } from 'pg';
+import { pool, connectToDb } from './connections.ts';
+
+await connectToDb();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Query database using COUNT() and GROUP BY
+pool.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', (err: Error, result: QueryResult) => {
+  if (err) {
+    console.log(err);
+  } else if (result) {
+    console.log(result.rows);
+  }
+});
+
+// Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
+pool.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', (err: Error, result: QueryResult) => {
+  if (err) {
+    console.log(err);
+  } else if (result) {
+    console.log(result.rows);
+  }
+});
+
+app.use((_req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
