346ce6e62650a1ea0c8dc8209055569fc6f9948b
zomblic <c.robinwork@outlook.com>
2025-03-02 15:29:52 -0500

    debugged files! YAY!

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+\c employees_db;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
